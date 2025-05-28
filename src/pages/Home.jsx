045107b820f28d6ed8c15cf82067f0748ae4634b
Yassine El koujjer <yassin123beta@gmail.com>
2025-05-28 10:22:13 +0200

    project starter

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    
+    {/* toggle btn */}
+
+    {/* backgound Effects */}
+
+    {/* Navbar */}
+
+    {/* main contents */}
+
+    {/* Footer */}
+    </>
+  )
+}
+
+export default Home
